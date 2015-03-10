407cbc39cefbc24b59f59dfb0f8c9fb48394460e
Rodrigo Turini <rdrg.turini@gmail.com>
2015-03-10 12:49:11 -0300

    sample integrating thymeleaf template engine in mvc

---
+++ ext/thymeleaf/src/main/java/com/oracle/ozark/test/thymeleaf/ThymeleafViewEngine.java
@@ -0,0 +1,98 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2014-2015 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * http://glassfish.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+package com.oracle.ozark.test.thymeleaf;
+
+import java.io.IOException;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.mvc.engine.ViewEngine;
+import javax.mvc.engine.ViewEngineContext;
+import javax.mvc.engine.ViewEngineException;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
+
+/**
+ * Class ThymeleafViewEngine.
+ *
+ * @author Rodrigo Turini
+ */
+@ApplicationScoped
+public class ThymeleafViewEngine implements ViewEngine {
+
+	private static final String VIEW_BASE = "/WEB-INF/views/";
+
+	@Inject
+	private ServletContext servletContext;
+
+	private TemplateResolver resolver;
+
+
+	public ThymeleafViewEngine() {
+		resolver = new ServletContextTemplateResolver();
+		resolver.setPrefix(VIEW_BASE);
+	}
+
+	@Override
+	public boolean supports(String view) {
+		return view.endsWith("html");
+	}
+
+	@Override
+	public void processView(ViewEngineContext context) throws ViewEngineException {
+		try {
+			TemplateEngine engine = new TemplateEngine();
+			engine.setTemplateResolver(resolver);
+			HttpServletRequest request = context.getRequest();
+			HttpServletResponse response = context.getResponse();
+			WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+			ctx.setVariables(context.getModels());
+			engine.process(context.getView(), ctx, response.getWriter());
+		} catch (IOException e) {
+			throw new ViewEngineException(e);
+		}
+	}
+}
