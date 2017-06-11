12544ac5cca5a372679115b5084e0c1b79a63670
Christian Kaltepoth <christian@kaltepoth.de>
2017-06-11 12:54:30 +0200

    Use latest promoted Glassfish 5 build for integration tests (#106)

--- .travis-build.sh
+++ .travis-build.sh
@@ -5,22 +5,22 @@ set -e
 if [ "$1" == "glassfish-bundled" ]; then
 
   mvn -Pbundled clean install -B -V
-  find ./test/ -name \*.war -exec cp {} ./glassfish4/glassfish/domains/domain1/autodeploy/ \;
-  glassfish4/bin/asadmin start-domain
+  find ./test/ -name \*.war -exec cp {} ./glassfish5/glassfish/domains/domain1/autodeploy/ \;
+  glassfish5/bin/asadmin start-domain
   sleep 120
   mvn -Pintegration -Dintegration.serverPort=8080 verify
-  glassfish4/bin/asadmin stop-domain
+  glassfish5/bin/asadmin stop-domain
 
 elif [ "$1" == "glassfish-module" ]; then
 
   mvn -P\!bundled,module clean install -B -V
-  cp ozark/target/ozark-*.jar ./glassfish4/glassfish/modules/
-  cp ~/.m2/repository/javax/mvc/javax.mvc-api/1.0-SNAPSHOT/*.jar ./glassfish4/glassfish/modules/
-  find ./test/ -name \*.war -exec cp {} ./glassfish4/glassfish/domains/domain1/autodeploy/ \;
-  glassfish4/bin/asadmin start-domain
+  cp ozark/target/ozark-*.jar ./glassfish5/glassfish/modules/
+  cp ~/.m2/repository/javax/mvc/javax.mvc-api/1.0-SNAPSHOT/*.jar ./glassfish5/glassfish/modules/
+  find ./test/ -name \*.war -exec cp {} ./glassfish5/glassfish/domains/domain1/autodeploy/ \;
+  glassfish5/bin/asadmin start-domain
   sleep 120
   mvn -Pintegration -Dintegration.serverPort=8080 verify
-  glassfish4/bin/asadmin stop-domain
+  glassfish5/bin/asadmin stop-domain
 
 else
   echo "Unknown test type: $1"
