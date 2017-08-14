b73cd4f76dad2701b6f2839fe72711eb4570a206
Christian Kaltepoth <christian@kaltepoth.de>
2017-08-14 08:26:05 +0200

    Initial version of the Jersey integration module

--- jersey/src/test/java/org/glassfish/ozark/jersey/model/OzarkModelProcessorTest.java
+++ jersey/src/test/java/org/glassfish/ozark/jersey/model/OzarkModelProcessorTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.glassfish.ozark.jersey;
+package org.glassfish.ozark.jersey.model;
 
 import org.glassfish.jersey.server.model.Resource;
 import org.glassfish.jersey.server.model.ResourceModel;
