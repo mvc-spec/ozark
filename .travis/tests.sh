534bca20569795fcec83c6736186c99413289039
Christian Kaltepoth <christian@kaltepoth.de>
2017-08-18 08:05:15 +0200

    Refactored travis build config

--- .travis/tests.sh
+++ .travis/tests.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
-set -e
+set -ev
 
-if [ "$1" == "glassfish-bundled" ]; then
+GLASSFISH_URL="http://download.oracle.com/glassfish/5.0/nightly/glassfish-5.0-web-b10-06_29_2017.zip"
 
-  mvn -Pbundled clean install -B -V
+if [ "${1}" == "glassfish-bundled" ]; then
+
+  curl -s -o glassfish5.zip "${GLASSFISH_URL}"
+  unzip -q glassfish5.zip
+  mvn -B -V -Pbundled clean install
   find ./test/ -name \*.war -exec cp {} ./glassfish5/glassfish/domains/domain1/autodeploy/ \;
   glassfish5/bin/asadmin start-domain
   sleep 120
   mvn -Pintegration -Dintegration.serverPort=8080 verify
   glassfish5/bin/asadmin stop-domain
 
-elif [ "$1" == "glassfish-module" ]; then
+elif [ "${1}" == "glassfish-module" ]; then
 
-  mvn -P\!bundled,module clean install -B -V
+  curl -s -o glassfish5.zip "${GLASSFISH_URL}"
+  unzip -q glassfish5.zip
+  mvn -B -V -P\!bundled,module clean install
   cp ozark/target/ozark-core-*.jar ./glassfish5/glassfish/modules/
   cp jersey/target/ozark-jersey-*.jar ./glassfish5/glassfish/modules/
   cp ~/.m2/repository/javax/mvc/javax.mvc-api/1.0-SNAPSHOT/*.jar ./glassfish5/glassfish/modules/
