fcda0b93a598844fd3a549b5f60e42901af6df21
Christian Kaltepoth <christian@kaltepoth.de>
2018-02-15 19:10:25 +0100

    Fixed Travis build to use correct spec version

--- .travis/tests.sh
+++ .travis/tests.sh
@@ -23,7 +23,7 @@ elif [ "${1}" == "glassfish-module" ]; then
   mvn -B -V -P\!bundled,module clean install
   cp core/target/ozark-core-*.jar ./glassfish5/glassfish/modules/
   cp jersey/target/ozark-jersey-*.jar ./glassfish5/glassfish/modules/
-  cp ~/.m2/repository/javax/mvc/javax.mvc-api/1.0-pr/*.jar ./glassfish5/glassfish/modules/
+  cp ~/.m2/repository/javax/mvc/javax.mvc-api/1.0-SNAPSHOT/*.jar ./glassfish5/glassfish/modules/
   find ./test/ -name \*.war -exec cp {} ./glassfish5/glassfish/domains/domain1/autodeploy/ \;
   glassfish5/bin/asadmin start-domain
   sleep 120
